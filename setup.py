7610207937b57d51b32b4955856acca52d5efcaf
mahmud60 <mahmud.hasan.igni@gmail.com>
2024-11-06 14:03:54 +0100

    setup file updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    description="A simple math quiz package",
+    author="Your Name",
+    author_email="your.email@example.com",
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here or leave empty if there are none
+)
